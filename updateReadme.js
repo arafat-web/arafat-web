64262578e36878c7d8d7ea656f5650bf30b0ec21
Arafat Hossain Ar <arafat.122260@gmail.com>
2024-05-27 20:59:11 +0600

    Create updateReadme.js

---
+++ updateReadme.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const Parser = require('rss-parser');
+const parser = new Parser();
+
+(async () => {
+  const feed = await parser.parseURL('https://dev.to/feed/arafatweb');
+  let readmeContent = fs.readFileSync('README.md', 'utf8');
+  let newBlogContent = '';
+  feed.items.slice(0, 5).forEach(item => {
+    newBlogContent += `## [${item.title}](${item.link})\n`;
+    newBlogContent += `![Image](${item.enclosure?.url || 'No image available'})\n`;
+    newBlogContent += `Description: ${item.contentSnippet}\n\n`;
+  });
+
+  const newReadme = readmeContent.replace(/<!-- BLOG-POST-LIST:START -->.*<!-- BLOG-POST-LIST:END -->/s, `<!-- BLOG-POST-LIST:START -->\n${newBlogContent}<!-- BLOG-POST-LIST:END -->`);
+  fs.writeFileSync('README.md', newReadme);
+})();
